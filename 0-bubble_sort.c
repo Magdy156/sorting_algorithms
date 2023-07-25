d32e5182a7753f44cb8b0dccd04ce135c1346883
Magdy156 <magdyhamdy156@gmail.com>
2023-07-26 00:04:15 +0300

    task 0

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -14,6 +14,8 @@ void bubble_sort(int *array, size_t size)
 	size_t i, j;
 	bool flag = true;
 
+	if (array == NULL || size < 2)
+		return;
 	for (i = 0; i < size - 1; i++)
 	{
 		for (j = 0; j < size - i - 1; j++)
