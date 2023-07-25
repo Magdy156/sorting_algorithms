ad840682725106b5d82f935517696e3d64edf31f
Magdy156 <magdyhamdy156@gmail.com>
2023-07-26 01:00:55 +0300

    task 1 |||

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -38,18 +38,18 @@ void insertion_sort_list(listint_t **list)
  */
 void swap_nodes(listint_t **h, listint_t **n1, listint_t *n2)
 {
-	(*n1)->next = n2->next;
 	if (n2->next)
 		n2->next->prev = *n1;
-	n2->prev = (*n1)->prev;
-	n2->next = *n1;
 
 	if ((*n1)->prev)
 		(*n1)->prev->next = n2;
+
 	else
 		*h = n2;
 
+	(*n1)->next = n2->next;
+	n2->prev = (*n1)->prev;
+	n2->next = *n1;
 	(*n1)->prev = n2;
 	*n1 = n2->prev;
-
 }
